c538c3f087975fe9ba36ab1733b42156375d4bd2
hdubey64 <hdubey64@gmail.com>
2022-11-16 13:23:34 +0530

    Fourth-tictoctoe c complete project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Icon from "./components/Icon";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import { Card, CardBody, Container, Button, Col, Row } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import { useState } from "react";
+import "./App.css";
+
+const itemArray = new Array(9).fill("empty");
+
+const App = () => {
+   const [isCross, setIsCross] = useState(false);
+   const [winMasssge, setWinMasssge] = useState("");
+
+   const reloadGame = () => {
+      setIsCross(false);
+      setWinMasssge("");
+      itemArray.fill("empty", 0, 9);
+   };
+
+   const checkIsWinner = () => {
+      // Checking Winner Of the Game
+      if (
+         itemArray[0] === itemArray[1] &&
+         itemArray[0] === itemArray[2] &&
+         itemArray[0] !== "empty"
+      ) {
+         setWinMasssge(`${itemArray[0]} won`);
+      } else if (
+         itemArray[3] !== "empty" &&
+         itemArray[3] === itemArray[4] &&
+         itemArray[4] === itemArray[5]
+      ) {
+         setWinMasssge(`${itemArray[3]} won`);
+      } else if (
+         itemArray[6] !== "empty" &&
+         itemArray[6] === itemArray[7] &&
+         itemArray[7] === itemArray[8]
+      ) {
+         setWinMasssge(`${itemArray[6]} won`);
+      } else if (
+         itemArray[0] !== "empty" &&
+         itemArray[0] === itemArray[3] &&
+         itemArray[3] === itemArray[6]
+      ) {
+         setWinMasssge(`${itemArray[0]} won`);
+      } else if (
+         itemArray[1] !== "empty" &&
+         itemArray[1] === itemArray[4] &&
+         itemArray[4] === itemArray[7]
+      ) {
+         setWinMasssge(`${itemArray[1]} won`);
+      } else if (
+         itemArray[2] !== "empty" &&
+         itemArray[2] === itemArray[5] &&
+         itemArray[5] === itemArray[8]
+      ) {
+         setWinMasssge(`${itemArray[2]} won`);
+      } else if (
+         itemArray[0] !== "empty" &&
+         itemArray[0] === itemArray[4] &&
+         itemArray[4] === itemArray[8]
+      ) {
+         setWinMasssge(`${itemArray[0]} won`);
+      } else if (
+         itemArray[6] !== "empty" &&
+         itemArray[6] === itemArray[4] &&
+         itemArray[4] === itemArray[2]
+      ) {
+         setWinMasssge(`${itemArray[6]} won`);
+      }
+   };
+
+   const changeItem = (itemNumber) => {
+      if (winMasssge) {
+         return toast(winMasssge, { type: "success" });
+      }
+      if (itemArray[itemNumber] === "empty") {
+         itemArray[itemNumber] = isCross ? "cross" : "circle";
+         setIsCross(!isCross);
+      } else {
+         return toast("already filled", { type: "error" });
+      }
+      checkIsWinner();
+   };
+   return (
+      <Container className="p-5">
+         <ToastContainer position="bottom-center" />
+         <Row>
+            <Col md={6} className="offset-md-3">
+               {winMasssge ? (
+                  <div className="mb-2 mt-2">
+                     <h1 className="text-success text-uppercase text-center">
+                        {winMasssge}
+                     </h1>
+                     <Button color="success" onClick={reloadGame}>
+                        Reload the Game
+                     </Button>
+                  </div>
+               ) : (
+                  <h1 className="text-center text-warning">
+                     {isCross ? "cross" : "circle"} turns
+                  </h1>
+               )}
+               <div className="grid">
+                  {itemArray.map((item, index) => (
+                     <Card color="warning" onClick={() => changeItem(index)}>
+                        <CardBody className="box">
+                           <Icon name={item} />
+                        </CardBody>
+                     </Card>
+                  ))}
+               </div>
+            </Col>
+         </Row>
+      </Container>
+   );
+};
 
 export default App;
